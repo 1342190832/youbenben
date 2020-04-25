19c585d5a92778116cc7106705b3ed13056d19e5
1342190832@qq.com <1342190832@qq.com>
2020-04-25 14:51:57 +0800

    第三次提交

---
+++ bizui/src/custcomponents/bizcomponents/leveltwocategory/LevelTwoCategory.associateform.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Card, Button, Form, Icon, Col, Row, DatePicker, TimePicker, Input, Select, Popover,Switch,Modal } from 'antd'
+import { connect } from 'dva'
+import PageHeaderLayout from '../../layouts/PageHeaderLayout'
+import {ImageComponent} from '../../axios/tools'
+import FooterToolbar from '../../components/FooterToolbar'
+import styles from './LevelTwoCategory.createform.less'
+import {mapBackToImageValues, mapFromImageValues} from '../../axios/tools'
+import GlobalComponents from '../../custcomponentsError#: ObjectCollection for youbenben failed with null
+
+
+
